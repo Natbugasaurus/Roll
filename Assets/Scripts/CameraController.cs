817884d5833496ae655466aee41a3914ddfecbd8
Nathaniel Bott <natbott97@gmail.com>
2018-03-26 14:24:23 +1100

    Fixed the camera and added level switching

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	private Vector3 offset;
-    public Transform target;
+	
+	public Transform target;
     public float rotateSpeed;
-	// Use this for initialization
+	float x = 0.0f;
+	float y = 0.0f;
+
+	public float yMin, yMax;
+
+	public bool invertY;
+
+	private Vector3 offset;
+
 	void Start () {
 		offset = transform.position - target.transform.position;
 	}
-
-	// Update is called once per frame
+		
 	void Update () {
-		transform.position = target.transform.position + offset;
-        transform.LookAt(target);
+        //if (Input.GetMouseButton(0)) {
+			x += Input.GetAxis ("Mouse X") * rotateSpeed;
+			y -= Input.GetAxis ("Mouse Y") * rotateSpeed;
+        //}
 
-        if (Input.GetMouseButton(0))
-        {
-            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up);
+		y = Mathf.Clamp (y, yMin, yMax);
 
-            offset = camTurnAngle * offset;
-        }
+		Quaternion rotation = Quaternion.Euler (y, x, 0);
+		transform.rotation = rotation;
+
+		transform.position = rotation * offset + target.transform.position ;
+		transform.LookAt(target);
 	}
 }
--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class LevelEnd : MonoBehaviour {
 	public Transform level;
+	public GameController gameController;
+
+	void start () {
+
+	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
 			other.transform.position = new Vector3(0,0,0);
 			level.transform.rotation =  Quaternion.identity;
+			gameController.NextLevel ();
 		}
 	}
 }
